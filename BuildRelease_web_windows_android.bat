9b0f9f69824ee056dbe8f17aa6d1a6560bfd3417
Toazd <wmcdannell@gmail.com>
2025-12-20 10:51:09 -0500

    update artifact names...again?

--- BuildRelease_web_windows_android.bat
+++ BuildRelease_web_windows_android.bat
@@ -3,7 +3,7 @@ REM No longer need since the migration from firebase to supabase
 REM CALL mkdir "C:\Users\wmcdannell\Documents\Bible Study\Apps\Selah_dev\selah\build\windows\x64"
 REM CALL copy /Y "C:\Users\wmcdannell\Documents\Bible Study\Apps\Selah_dev\_build_windows_x64\firebase_cpp_sdk_windows_12.7.0.zip" "C:\Users\wmcdannell\Documents\Bible Study\Apps\Selah_dev\selah\build\windows\x64"
 
-cd "C:\Users\wmcdannell\Documents\Bible Study\Apps\Selah_dev\selah"
+REM cd "C:\Users\wmcdannell\Documents\Bible Study\Apps\Selah_dev\selah"
 CALL flutter build web --release
 CALL flutter build windows --release
 CALL flutter build apk --release
