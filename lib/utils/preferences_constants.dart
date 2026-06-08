e52f77aeb392a565cf10a37159fda7a1355a0178
toazd <wmcdannell@gmail.com>
2026-06-08 08:49:28 -0400

    Change default light mode background color slightly. Update dependencies.

--- lib/utils/preferences_constants.dart
+++ lib/utils/preferences_constants.dart
@@ -11,7 +11,8 @@ const double defaultHighlightAlpha = 0.90; // Not user-configurable
 
 // Color defaults in hex
 const String defaultLightPrimaryColorHex = '#FF607D8B';
-const String defaultLightBackgroundColorHex = '#FFFFF7CB';
+const String defaultLightBackgroundColorHex =
+    '#FFFFFFE3'; //'#FFFCFAEB'; //'#FFFFF7CB';
 const String defaultLightTextColorHex = '#FF000000';
 const String defaultDarkPrimaryColorHex = '#FF607D8B';
 const String defaultDarkBackgroundColorHex = '#FF242424'; //'#FF000010';
