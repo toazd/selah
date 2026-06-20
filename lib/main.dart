b082f9decd31b750acc45e6b1501ef9a10c79562
toazd <wmcdannell@gmail.com>
2026-06-20 08:41:46 -0400

    bumpity bump

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ import 'utils/tablet_mode_detector.dart';
 import 'utils/error_handler.dart';
 import 'package:flutter/rendering.dart';
 
-final appVersion = "0.7.13";
+final appVersion = "0.7.14";
 
 final ValueNotifier<ThemeMode> themeModeNotifier = ValueNotifier(
   ThemeMode.system,
